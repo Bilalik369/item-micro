3cc6fac7e704fe50f238c7183484d4afcc80a3a3
bilal iken <bilal11iken@gmail.com>
2025-10-28 16:12:20 +0100

    ajoute de foncation pour ajoute toutes les elemente

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -15,3 +15,55 @@ export const createItem = async(req ,res)=>{
         return res.status(500).json({msg : "Failed to create item"})
     }
 } 
+
+export const getAllItems = async(req , res)=>{
+    try {
+        const {
+      page = 1,
+      limit = 10,
+      category,
+      city,
+      minPrice,
+      maxPrice,
+      availability,
+      search,
+      sortBy = "createdAt",
+      order = "desc",
+    } = req.query
+
+    const filter = {isActive : true}
+    if(category) filter.category = category
+    if (city) filter["location.city"] = new RegExp(city, "i")
+     if (availability) filter.availability = availability
+
+
+    if(minPrice || maxPrice){
+        filter.pricePerDay = {}
+        if (minPrice) filter.pricePerDay.$gte = Number(minPrice)
+        if (maxPrice) filter.pricePerDay.$lte = Number(maxPrice)
+    }
+    if (search) {
+      filter.$text = { $search: search }
+    }
+    const sortOptions = {}
+    sortOptions[sortBy] = order === "asc" ? 1 : -1
+
+    const items = await Item.find(filter)
+      .sort(sortOptions)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+
+    const count = await Item.countDocuments(filter)
+    return res.status(200).json({
+        items, 
+        totalPages: Math.ceil(count / limit),
+        currentPage: Number(page),
+        totalItems: count,
+    })
+
+
+    } catch (error) {
+        console.error("Get all items error:", error)
+        return res.status(500).json({msg: "Failed to fetch items"})
+    }
+}
--- routes/item.route.js
+++ routes/item.route.js
@@ -1,11 +1,12 @@
 import express from "express"
-import {createItem } from "../controllers/item.controller.js"
+import {createItem, getAllItems } from "../controllers/item.controller.js"
 import {authMiddleware} from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 
 router.post("/" , authMiddleware ,createItem)
+router.get("/" , authMiddleware ,getAllItems)
 
 
 
